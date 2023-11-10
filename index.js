bbd93c3e04e956142bbdffcadc8c7e0e50bcea81
Joel Huckle <joelhuckle2004@gmail.com>
2023-11-10 15:52:15 +0000

    added extensions object

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const fs = require("fs");
+const path = require("path");
+
+const extensions = {
+  ".docx": "Documents",
+  ".pdf": "Documents",
+  ".mp3": "Audio",
+  ".wav": "Audio",
+  ".png": "Images",
+  ".jpeg": "Images",
+  ".zip": "Zips",
+  ".flp": "Flps",
+};
+
+//for each file in folder,
+//if file in Object.values(extensions)
+//if not, get extension and add to respective folder
+//if not in extensions, add to Unspecified
+
+let files = [];
+
+fs.readdir("/Users/joelhuckle/Downloads", "", (err, data) => {
+  if (err) throw err;
+  data.forEach((file) => {
+    const ext = path.extname(file);
+    if (ext) {
+    }
+  });
+});
