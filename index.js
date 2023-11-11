325b61a50c9e09522d18f9dca363b32e62026365
Joel Huckle <joelhuckle2004@gmail.com>
2023-11-11 19:17:51 +0000

    fs rename to move files

--- index.js
+++ index.js
@@ -12,18 +12,22 @@ const extensions = {
   ".flp": "Flps",
 };
 
-//for each file in folder,
-//if file in Object.values(extensions)
-//if not, get extension and add to respective folder
-//if not in extensions, add to Unspecified
-
-let files = [];
-
 fs.readdir("/Users/joelhuckle/Downloads", "", (err, data) => {
   if (err) throw err;
   data.forEach((file) => {
     const ext = path.extname(file);
     if (ext) {
+      const oldPath = path.join("/Users/joelhuckle/Downloads", file);
+      const newPath = path.join(
+        "/Users/joelhuckle/Downloads",
+        extensions[ext] || "Other",
+        file
+      );
+
+      fs.rename(oldPath, newPath, function (err) {
+        if (err) throw err;
+        console.log(`${file} moved to ${newPath}`);
+      });
     }
   });
 });
